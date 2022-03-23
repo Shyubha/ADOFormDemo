23adc5fe7eb3e83f006c579ae6218465124eea63
PrashantSuryawanshi <suryawanshiprashant026@gmail.com>
2022-03-23 18:33:34 +0530

    Add project files.

---
+++ ADONetDemo/Form1.Designer.cs
@@ -0,0 +1,198 @@
+﻿namespace ADONetDemo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnAddNew = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.txtId = new System.Windows.Forms.TextBox();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtDesignation = new System.Windows.Forms.TextBox();
+            this.txtSalary = new System.Windows.Forms.TextBox();
+            this.IdLable = new System.Windows.Forms.Label();
+            this.DesignLable = new System.Windows.Forms.Label();
+            this.NameLable = new System.Windows.Forms.Label();
+            this.Salarylabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(312, 370);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 25;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(453, 370);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(75, 23);
+            this.btnUpdate.TabIndex = 24;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            // 
+            // btnAddNew
+            // 
+            this.btnAddNew.Location = new System.Drawing.Point(151, 370);
+            this.btnAddNew.Name = "btnAddNew";
+            this.btnAddNew.Size = new System.Drawing.Size(101, 23);
+            this.btnAddNew.TabIndex = 23;
+            this.btnAddNew.Text = "Add New";
+            this.btnAddNew.UseVisualStyleBackColor = true;
+            this.btnAddNew.Click += new System.EventHandler(this.btnAddNew_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(577, 108);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(75, 23);
+            this.btnDelete.TabIndex = 22;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(577, 57);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(75, 23);
+            this.btnSearch.TabIndex = 21;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            // 
+            // txtId
+            // 
+            this.txtId.Location = new System.Drawing.Point(331, 58);
+            this.txtId.Name = "txtId";
+            this.txtId.Size = new System.Drawing.Size(164, 22);
+            this.txtId.TabIndex = 20;
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(331, 124);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(164, 22);
+            this.txtName.TabIndex = 19;
+            // 
+            // txtDesignation
+            // 
+            this.txtDesignation.Location = new System.Drawing.Point(331, 199);
+            this.txtDesignation.Name = "txtDesignation";
+            this.txtDesignation.Size = new System.Drawing.Size(164, 22);
+            this.txtDesignation.TabIndex = 18;
+            // 
+            // txtSalary
+            // 
+            this.txtSalary.Location = new System.Drawing.Point(331, 275);
+            this.txtSalary.Name = "txtSalary";
+            this.txtSalary.Size = new System.Drawing.Size(164, 22);
+            this.txtSalary.TabIndex = 17;
+            // 
+            // IdLable
+            // 
+            this.IdLable.AutoSize = true;
+            this.IdLable.Location = new System.Drawing.Point(148, 64);
+            this.IdLable.Name = "IdLable";
+            this.IdLable.Size = new System.Drawing.Size(18, 16);
+            this.IdLable.TabIndex = 16;
+            this.IdLable.Text = "Id";
+            // 
+            // DesignLable
+            // 
+            this.DesignLable.AutoSize = true;
+            this.DesignLable.Location = new System.Drawing.Point(148, 202);
+            this.DesignLable.Name = "DesignLable";
+            this.DesignLable.Size = new System.Drawing.Size(79, 16);
+            this.DesignLable.TabIndex = 15;
+            this.DesignLable.Text = "Designation";
+            // 
+            // NameLable
+            // 
+            this.NameLable.AutoSize = true;
+            this.NameLable.Location = new System.Drawing.Point(148, 127);
+            this.NameLable.Name = "NameLable";
+            this.NameLable.Size = new System.Drawing.Size(44, 16);
+            this.NameLable.TabIndex = 14;
+            this.NameLable.Text = "Name";
+            // 
+            // Salarylabel
+            // 
+            this.Salarylabel.AutoSize = true;
+            this.Salarylabel.Location = new System.Drawing.Point(148, 281);
+            this.Salarylabel.Name = "Salarylabel";
+            this.Salarylabel.Size = new System.Drawing.Size(46, 16);
+            this.Salarylabel.TabIndex = 13;
+            this.Salarylabel.Text = "Salary";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnAddNew);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.txtId);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.txtDesignation);
+            this.Controls.Add(this.txtSalary);
+            this.Controls.Add(this.IdLable);
+            this.Controls.Add(this.DesignLable);
+            this.Controls.Add(this.NameLable);
+            this.Controls.Add(this.Salarylabel);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnAddNew;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.TextBox txtId;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtDesignation;
+        private System.Windows.Forms.TextBox txtSalary;
+        private System.Windows.Forms.Label IdLable;
+        private System.Windows.Forms.Label DesignLable;
+        private System.Windows.Forms.Label NameLable;
+        private System.Windows.Forms.Label Salarylabel;
+    }
+}
+
---
+++ ADONetDemo/Form1.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace ADONetDemo
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection con;
+        SqlCommand cmd;
+        SqlDataReader dr;
+        public Form1()
+        {
+            InitializeComponent();
+            con = new SqlConnection("Server=LAPTOP-J8NQSAO4;DataBase=PracticeDB;Integrated Security =true");
+        }
+        private void ClearData()
+        {
+            txtId.Clear();
+            txtName.Clear();
+            txtDesignation.Clear();
+            txtSalary.Clear();
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //To pas values into the insert query we will use parameters (parametric query)
+                string qry = "insert into Employee Values(@id,@name,@designation,@salary)";
+                cmd = new SqlCommand(qry, con);
+                con.Open();
+                cmd.Parameters.AddWithValue("@id", Convert.ToInt32(txtId.Text));
+                cmd.Parameters.AddWithValue("@name", txtName.Text);
+                cmd.Parameters.AddWithValue("@designation", txtDesignation.Text);
+                cmd.Parameters.AddWithValue("@salary", Convert.ToDouble(txtSalary.Text));
+                int result = cmd.ExecuteNonQuery();
+                if (result == 1)
+                {
+                    MessageBox.Show("Successfully saved the record");
+                    ClearData();
+                }
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                con.Close();
+            }
+        }
+        private void btnAddNew_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string qry = "select MAX(Id) from Employee";
+                cmd = new SqlCommand(qry, con);
+                con.Open();
+                object obj = cmd.ExecuteScalar();
+                // DBNullis used to check existance of value in the obj
+                if (obj == DBNull.Value)
+                {
+                    txtId.Text = "1";
+                }
+                else
+
+                {
+                    int id = Convert.ToInt32(obj);//will get the MAX Id from employee
+                    id++;
+                    txtId.Text = id.ToString();
+
+                }
+                txtId.Enabled = false;//now textbox value cannot 
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                ClearData();
+            }
+            finally
+            {
+                con.Close();
+            }
+        }
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //To pas values into the insert query we will use parameters (parametric query)
+                string qry = "update Employee set Name=@name,Designation=@designation,Salary=@salary where Id=@id";
+                cmd = new SqlCommand(qry, con);
+                con.Open();
+                cmd.Parameters.AddWithValue("@id", Convert.ToInt32(txtId.Text));
+                cmd.Parameters.AddWithValue("@name", txtName.Text);
+                cmd.Parameters.AddWithValue("@designation", txtDesignation.Text);
+                cmd.Parameters.AddWithValue("@salary", Convert.ToDouble(txtSalary.Text));
+                int result = cmd.ExecuteNonQuery();
+                if (result == 1)
+                {
+                    MessageBox.Show("Successfully Update the record");
+                    ClearData();
+                }
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                con.Close();
+            }
+
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string qry = "select*from Employee where Id = @id";
+                cmd = new SqlCommand(qry, con);
+                con.Open();
+                cmd.Parameters.AddWithValue("@id", Convert.ToInt32(txtId.Text));
+                dr = cmd.ExecuteReader();
+                //first will check that record is present
+                if (dr.HasRows)
+                {
+                    //read the record from dr obj
+                    if (dr.Read())
+                    {
+                        txtName.Text = dr["Name"].ToString();
+                        txtDesignation.Text = dr["Designation"].ToString();
+                        txtSalary.Text = dr["Salary"].ToString();
+
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Record not found");
+
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                con.Close();
+            }
+
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //To pas values into the insert query we will use parameters (parametric query)
+                string qry = "delete from Employee where Id=@id";
+                cmd = new SqlCommand(qry, con);
+                con.Open();
+                cmd.Parameters.AddWithValue("@id", Convert.ToInt32(txtId.Text));
+
+                int result = cmd.ExecuteNonQuery();
+                if (result == 1)
+                {
+                    MessageBox.Show("Successfully delete the record");
+                    ClearData();
+                }
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                con.Close();
+            }
+
+        }
+
+    }
+}
